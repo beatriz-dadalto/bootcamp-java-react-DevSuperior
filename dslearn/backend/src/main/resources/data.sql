77d7c17a3b808a82e710a1d0687b54aff0e8d767
Bia Coelho <biacoelho@pm.me>
2021-07-03 01:10:02 -0300

    Resource entity

--- dslearn/backend/src/main/resources/data.sql
+++ dslearn/backend/src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-06-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-06-13T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-12-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-13T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://www.euclid-project.eu/sites/default/files/images/html_icon.png', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://www.euclid-project.eu/sites/default/files/images/html_icon.png', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://www.euclid-project.eu/sites/default/files/images/html_icon.png', 0, 1);
